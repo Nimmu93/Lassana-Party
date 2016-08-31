117ef04f60542cfd14fbd1861f4b5d5cebc638fa
letmadsay <desilvamadhuka@gmail.com>
2016-08-31 19:13:42 +0530

    changed contact details in home page and removed zoom property

--- assets/js/jquery.visible.js
+++ assets/js/jquery.visible.js
@@ -1,14 +1,9 @@
 (function($){
 
     /**
-* Copyright 2012, Digital Fusion
-* Licensed under the MIT license.
-* http://teamdf.com/jquery-plugins/license/
-*
-* @author Sam Sehnert
-* @desc A small plugin that checks whether elements are within
-* the user visible viewport of a web browser.
-* only accounts for vertical position, not horizontal.
+ A small plugin that checks whether elements are within
+the user visible viewport of a web browser.
+only accounts for vertical position, not horizontal.
 */
     var $w = $(window);
     $.fn.visible = function(partial,hidden,direction){
